4f48b6e2117b4d01826e29dab0e781d4591c7497
Lynn McKay <iamlynnmckay@gmail.com>
2021-12-27 19:40:39 -0800

    Fix formula for 'dotfiles'

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -1,11 +1,21 @@
 class Dotfiles < Formula
     desc ""
     homepage "https://github.com/iamlynnmckay/dotfiles"
-    url "https://github.com/iamlynnmckay/dotfiles/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "987df80e9b03f0152a73add1ae5bc7690ea8153fcb4e27336d998354d5ee70e9"
+    url "https://github.com/iamlynnmckay/dotfiles/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "fbcefd0df06798195d5395bd35fa55be6fc7276174b817a0479eb3f62db2c226"
     license "MIT"
     def install
-      system "make"
+        bin.install "main.sh" => "dotfiles"
+        prefix.install Dir["src/*"]
+    end
+    def caveats
+        <<~EOS
+
+            Bootstrap successful, dotfiles are available in /usr/local/opt/dotfiles.
+
+            To complete the installation, run 'dotfiles --install /usr/local/opt/dotfiles'.
+
+        EOS
     end
     test do
       system "true"
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# homebrew-iamlynnmckay
+# README
+
+Custom homebrew tap by Lynn McKay.
+
+For more information, see some of the following links.
+
+- https://docs.brew.sh/Taps
+- https://docs.brew.sh/How-to-Create-and-Maintain-a-Tap
+- https://docs.brew.sh/Formula-Cookbook
+- https://betterprogramming.pub/a-step-by-step-guide-to-create-homebrew-taps-from-github-repos-f33d3755ba74
