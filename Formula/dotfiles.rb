f6a35e700e09a97bb8356382ce88858c456ba210
Lynn McKay <iamlynnmckay@gmail.com>
2021-12-28 21:17:01 -0800

    Update dotfiles to v1.0.3

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -1,8 +1,8 @@
 class Dotfiles < Formula
     desc ""
     homepage "https://github.com/iamlynnmckay/dotfiles"
-    url "https://github.com/iamlynnmckay/dotfiles/archive/refs/tags/v1.0.2.tar.gz"
-    sha256 "438bf401b4189e60467f27abb267b9362c1e912f518abd532364dc61dfcdb735"
+    url "https://github.com/iamlynnmckay/dotfiles/archive/refs/tags/v1.0.3.tar.gz"
+    sha256 "6fd2a5da9a60207f7bcb8205e71dedc766535b5b4644195552a86c56f9ff8244"
     license "MIT"
     def install
         bin.install "main.sh" => "dotfiles"
