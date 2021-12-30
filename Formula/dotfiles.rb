c2503e896dfd598ba3cdbce7132d013401bc2acd
Lynn McKay <iamlynnmckay@gmail.com>
2021-12-29 19:05:00 -0800

    Remove sha256 checksums

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -2,7 +2,7 @@ class Dotfiles < Formula
     desc ""
     homepage "https://github.com/iamlynnmckay/dotfiles"
     url "https://github.com/iamlynnmckay/dotfiles/archive/refs/tags/v1.0.3.tar.gz"
-    sha256 "6fd2a5da9a60207f7bcb8205e71dedc766535b5b4644195552a86c56f9ff8244"
+    sha256 ""
     license "MIT"
     def install
         bin.install "main.sh" => "dotfiles"
