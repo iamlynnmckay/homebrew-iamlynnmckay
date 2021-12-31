4b01ab8ad5c0139715f020a1bfc7ddcd44ab095f
Lynn McKay <iamlynnmckay@gmail.com>
2021-12-30 20:24:36 -0800

    Debugging...

--- Formula/sfw.rb
+++ Formula/sfw.rb
@@ -4,7 +4,7 @@ class Sfw < Formula
     url "https://github.com/iamlynnmckay/sfw/archive/refs/tags/v1.0.0.tar.gz"
     sha256 ""
     license "MIT"
-    # formula
+    # formula dependencies
     depends_on "coreutils"
     depends_on "docker"
     depends_on "docker-compose"
@@ -19,21 +19,24 @@ class Sfw < Formula
     depends_on "haskell-stack"
     depends_on "neovim"
     depends_on "rust"
-    # cask
-    depends_on "homebrew/cask/firefox"
-    depends_on "homebrew/cask/gimp"
-    depends_on "homebrew/cask/google-chrome"
-    depends_on "homebrew/cask/intellij-idea"
-    depends_on "homebrew/cask/iterm2"
-    depends_on "homebrew/cask/libreoffice"
-    depends_on "homebrew/cask/mactex-no-gui"
-    depends_on "homebrew/cask/macvim"
-    depends_on "homebrew/cask/tmux"
-    depends_on "homebrew/cask/visual-studio-code"
-    depends_on "homebrew/cask/vlc"
-    # tap iamlynnmckay
+    # iamlynnmckay tap dependencies
     depends_on "iamlynnmckay/iamlynnmckay/dotfiles"
+    def pre_install
+        # cask dependencies
+        system "/usr/local/bin/brew", "install", "--cask", "firefox"
+        system "/usr/local/bin/brew", "install", "--cask", "gimp"
+        system "/usr/local/bin/brew", "install", "--cask", "google-chrome"
+        system "/usr/local/bin/brew", "install", "--cask", "intellij-idea"
+        system "/usr/local/bin/brew", "install", "--cask", "iterm2"
+        system "/usr/local/bin/brew", "install", "--cask", "libreoffice"
+        system "/usr/local/bin/brew", "install", "--cask", "mactex-no-gui"
+        system "/usr/local/bin/brew", "install", "--cask", "macvim"
+        system "/usr/local/bin/brew", "install", "--cask", "tmux"
+        system "/usr/local/bin/brew", "install", "--cask", "visual-studio-code"
+        system "/usr/local/bin/brew", "install", "--cask", "vlc"
+    end
     def install
+        # sfw executable
         bin.install "sfw"
     end
     def caveats
