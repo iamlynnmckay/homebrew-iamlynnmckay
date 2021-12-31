9d379f19e0ef6c5179ec5dd21b2a13f65273d3ec
Lynn McKay <iamlynnmckay@gmail.com>
2021-12-30 19:29:32 -0800

    Add dependecies in sfw and nsfw

--- Formula/nsfw.rb
+++ Formula/nsfw.rb
@@ -4,7 +4,32 @@ class Nsfw < Formula
     url "https://github.com/iamlynnmckay/nsfw/archive/refs/tags/v1.0.0.tar.gz"
     sha256 ""
     license "MIT"
-    depends_on "iamlynnmckay/iamlynnmckay/sfw"
+    #
+    depends_on cask: arduino
+    depends_on cask: audacity
+    depends_on cask: bitwig-studio
+    depends_on cask: blender
+    depends_on cask: coqide
+    depends_on cask: cycling74-max
+    depends_on cask: dexed
+    depends_on cask: discord
+    depends_on cask: dropbox
+    depends_on cask: emacs
+    depends_on cask: epic-games
+    depends_on cask: google-earth-pro
+    depends_on cask: pd
+    depends_on cask: rekordbox
+    depends_on cask: signal
+    depends_on cask: skype
+    depends_on cask: spotify
+    depends_on cask: steam
+    depends_on cask: supercollider
+    depends_on cask: twitch
+    depends_on cask: vcv-rack
+    depends_on cask: zoom
+    #
+    depends_on formula: "iamlynnmckay/iamlynnmckay/sfw"
+    #
     def install
         bin.install "nsfw"
     end
--- Formula/sfw.rb
+++ Formula/sfw.rb
@@ -4,7 +4,36 @@ class Sfw < Formula
     url "https://github.com/iamlynnmckay/sfw/archive/refs/tags/v1.0.0.tar.gz"
     sha256 ""
     license "MIT"
-    depends_on "iamlynnmckay/iamlynnmckay/dotfiles"
+    #
+    depends_on formula: coreutils
+    depends_on formula: docker
+    depends_on formula: docker-compose
+    depends_on formula: findutils
+    depends_on formula: gawk
+    depends_on formula: gnu-getopt
+    depends_on formula: gnu-indent
+    depends_on formula: gnu-sed
+    depends_on formula: gnu-tar
+    depends_on formula: gnutls
+    depends_on formula: grep
+    depends_on formula: haskell-stack
+    depends_on formula: neovim
+    depends_on formula: rust
+    #
+    depends_on cask: firefox
+    depends_on cask: gimp
+    depends_on cask: google-chrome
+    depends_on cask: intellij-idea
+    depends_on cask: iterm2
+    depends_on cask: libreoffice
+    depends_on cask: mactex-no-gui
+    depends_on cask: macvim
+    depends_on cask: tmux
+    depends_on cask: visual-studio-code
+    depends_on cask: vlc
+    #
+    depends_on formula: "iamlynnmckay/iamlynnmckay/dotfiles"
+    #
     def install
         bin.install "sfw"
     end
